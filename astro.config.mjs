48566f66be2d7268d85107a1ff984299c29aa881
nielsjacobs <niels@zimmo.be>
2023-11-23 07:52:49 +0100

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
 		functionPerRoute: false,
 	}),
 	// eslint-disable-next-line no-undef
-	site: process.env.SITE_URL,
+	site: "hrwizard-7jeev3owg-jacobsniels10.vercel.app",
 	vite: {
 		plugins: [
 			checker({
